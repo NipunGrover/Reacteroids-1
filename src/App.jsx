6519200fb3ad57a5dd6b00ccfdbe2b41a549e931
Anzhelika Kostyuk <akostyuk@conestogac.on.ca>
2024-05-16 16:23:11 -0400

    VCON-574 Added a space background to the start menu VCON-574 Added buttons to choose the side

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import { Reacteroids } from './components/Reacteroids'
+import Menu from './components/StartMenu/Menu'
 
 function App() {
   return (
     <>
-      <Reacteroids/>
+      <Menu/>
     </>
   )
 }
--- src/components/StartMenu/Menu.jsx
+++ src/components/StartMenu/Menu.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import Starfield from 'react-starfield';
 
-const Menu = () => {
+export default function Menu({onClick}){
+  
   return (
     <>
-        <div>
-      <header>
-        <h1>My Simple React Home Page</h1>
-      </header>
-      <main>
-        <p>Welcome to my simple React home page! This is a basic example of a React project.</p>
-        <img src="https://via.placeholder.com/300" alt="Placeholder" />
-      </main>
+      <Starfield
+        starCount={1000}
+        starColor={[255, 255, 255]}
+        speedFactor={0.05}
+        backgroundColor="black"
+      />
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-16 z-1 text-center">
+        <h1 className="text-5xl">Choose your side</h1>
+        <br></br>
+        <button onClick={onClick} className="border-4 box-border border-white bg-transparent text-white text-m px-10 py-5 m-5 cursor-pointer hover:shadow-2xl hover:shadow-lime-500 hover:border-lime-500 hover:bg-lime-500">REBEL</button>
+        <button onClick={onClick} className="border-4 box-border border-white bg-transparent text-white text-m px-10 py-5 m-5 cursor-pointer hover:shadow-2xl hover:shadow-red-500 hover:border-red-500 hover:bg-red-500">SITH</button>
       </div>
+
     </>
   );
 }
-
-export default Menu;
